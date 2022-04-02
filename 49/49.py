d4c9a3bc87551caa6ef985d735512e107cb9ae1e
Tony Locke <tlocke@tlocke.org.uk>
2022-04-02 10:51:15 +0100

    Added 49

--- 49/49.py
+++ 49/49.py
@@ -1,4 +1,9 @@
-from itertools import combinations, dropwhile, permutations
+from itertools import (
+    combinations,
+    combinations_with_replacement,
+    dropwhile,
+    permutations,
+)
 
 primes = [2]
 
@@ -9,32 +14,11 @@ for i in range(3, 10000, 2):
 
 found = []
 
-for comb in combinations("".join(str(i) for i in range(10)), 4):
-    print(comb)
-    perms = [int("".join(p)) for p in permutations(comb, 4)]
-    print("perms", perms)
+for comb in combinations_with_replacement("".join(str(i) for i in range(10)), 4):
+    perms = set(int("".join(p)) for p in permutations(comb, 4) if p[0] != "0")
     perms = [p for p in perms if p in primes]
-    print("prime perms", perms)
     for c in combinations(perms, 3):
-        # print("c", c)
         if c[2] - c[1] == c[1] - c[0]:
             found.append(c)
 
-    """
-    if len(found) > 1:
-        # print("found", found)
-        break
-    """
-
-    """
-    if "0" in comb and "9" in comb:
-        break
-    """
-
-    """
-    if 1487 in perms:
-        print("1487", perms, "found", found)
-        break
-    """
-
-print("end found", found)
+print("end found", found, "".join(str(f) for f in found[-1]))
